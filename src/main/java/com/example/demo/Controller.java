b71432f58abc81e6b6e3c6b918da03bcf11cb7be
Tishya12 <goyaltishya@gmail.com>
2021-02-24 13:37:23 +0530

    Oauth demo

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello world";
+    }
+
+
+    @GetMapping("/restricted")
+    public String restricted() {
+        return "restricted";
+    }
+}
